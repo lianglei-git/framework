d0bcb669c295de301b492955a6a188fdaf9ca359
梁磊 <lei.liang@yizhun-ai.com>
2024-01-21 19:20:26 +0800

    .

--- Js/utils/DelayCmp.react.tsx
+++ Js/utils/DelayCmp.react.tsx
@@ -1,14 +1,32 @@
 import { useEffect, useState } from "react";
 
+const cacheMap = {};
 const DelayCmp = (props) => {
-  const { Component, delay } = props;
+  let { Component, delay } = props;
+  // if (props.cache) {
+  //   if (cacheMap[props.cache]) {
+  //     Component = cacheMap[props.cache];
+  //   } else {
+  //     cacheMap[props.cache] = Component;
+  //   }
+  // }
   const [show, SetShow] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       SetShow(true);
     }, delay * 1000);
   }, []);
-  return <div style={{ display: show ? "block" : "none" }}>{Component}</div>;
+  return (
+    <div
+      style={{
+        display: show ? "block" : "none",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      {Component}
+    </div>
+  );
 };
 
 export { DelayCmp };
